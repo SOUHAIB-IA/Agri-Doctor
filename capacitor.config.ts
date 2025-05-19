6a8dd6bba0d8c34cf8069745ca6c9aa8b04a2017
SOUHAIB-IA <sohaib,garaaouch55@gmail.com>
2025-05-19 17:37:52 +0100

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.agridoctor',
+  appName: 'agridoctor',
+  webDir: 'dist'
+};
+
+export default config;
